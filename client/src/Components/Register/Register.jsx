3068fbd531ac5eae7082d28143119825b4299e84
mspirovv <mspirov@abv.bg>
2025-03-24 08:45:25 +0200

    Updated register  with functionality

--- client/src/Components/Register/Register.jsx
+++ client/src/Components/Register/Register.jsx
@@ -1,51 +1,62 @@
-import React from "react";
+
+import { useRegister } from '../../services/ApiService';
+// import AuthContext from "../../contexts/authContext";
 import './Register.css'
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
+export default function Register() {
+
+  const { register } = useRegister()
+
 const schema = yup.object().shape({
-  username: yup.string().required("Username is required"),
   email: yup.string().email("Invalid email").required("Email is required"),
   password: yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
+  confirmPassword: yup .string() .oneOf([yup.ref("password"), null], "Passwords must match!") .required("Confirm Password is required!S"),
 });
 
-const Register = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
+  const registerHandler = async (formData) => {
+    const { email,password } = Object.fromEntries(formData);
+    const confirmPassword = formData.get('confirm-password');
+    console.log(email);
+    console.log(password);
+    console.log(confirmPassword);
+    
+    // const confirmPassword = formData.get('confirm-password');
 
-  const onSubmit = (data) => {
-    console.log(data);
-  };
+    if (password !== confirmPassword) {
+      console.log('Password missmatch!');
+
+      return;
+    }
+    const authData = await register(email,password);
+    console.log ('reg succesfull!')
+  }
 
   return (
- 
+      <>
     <div className="register-container">
       <div className="form-box">
         <h2 className="title">Register</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form action={registerHandler}>
           <div className="input-group">
-            <label>Username</label>
-            <input type="text" {...register("username")} className="input-field" />
-            {errors.username && <p className="error-text">{errors.username.message}</p>}
+            <label htmlFor='email'>Email:</label>
+            <input type="email" id='email' name='email' className="input-field" />
+            {/* {errors.email && <p className="error-text">{errors.email.message}</p>} */}
           </div>
 
           <div className="input-group">
-            <label>Email</label>
-            <input type="email" {...register("email")} className="input-field" />
-            {errors.email && <p className="error-text">{errors.email.message}</p>}
+            <label htmlFor='password'>Password:</label>
+            <input type="password" id='password' name='password' className="input-field" />
+            {/* {errors.password && <p className="error-text">{errors.password.message}</p>} */}
           </div>
 
           <div className="input-group">
-            <label>Password</label>
-            <input type="password" {...register("password")} className="input-field" />
-            {errors.password && <p className="error-text">{errors.password.message}</p>}
-          </div>
+            <label htmlFor='confirm-pasword'>Confirm Password:</label>
+            <input type="password" id='confirm-password' name='confirm-password' className="input-field" />
+            {/* {errors.confirmPassword && <p className="error-text">{errors.confirmPassword.message}</p>} */}
+            </div>
 
           <button type="submit" className="submit-btn">
             Register
@@ -53,8 +64,7 @@ const Register = () => {
         </form>
       </div>
     </div>
-
+    </>
   );
-};
 
-export default Register;
+}
