698061453122a500c3cf521154f7720ad031e007
mspirovv <mspirov@abv.bg>
2025-03-25 09:41:50 +0200

    added function to get all cars

--- client/src/services/carService.js
+++ client/src/services/carService.js
@@ -4,6 +4,17 @@ import useAuth from "../hooks/useAuth";
 
 const baseUrl = 'http://localhost:3030/data/cars';
 
+export const useCars = () => {
+    const [cars,setCars] = useState([]);
+
+    useEffect(() => {
+        request.get(baseUrl)
+        .then(setCars)
+    }, []);
+
+    return { cars };
+};
+
 export const useCreateCar = () => {
     const { request } = useAuth();
 
