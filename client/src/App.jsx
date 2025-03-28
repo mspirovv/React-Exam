6f6043878313eaed65e7512da870fe918d38d12e
mspirovv <mspirov@abv.bg>
2025-03-28 10:08:50 +0200

    added GuestGuard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import CarDetails from './Components/car-details/CarDetails';
 import CarEdit from './Components/car-edit/CarEdit';
 import Logout from './Components/logout/Logout';
 import AuthGuard from './Components/guards/AuthGuard';
+import GuestGuard from './Components/guards/GuestGuard';
 
 function App() {
 
@@ -32,13 +33,18 @@ function App() {
       <Routes>
         <Route index element={<Home/>} />
         <Route path='/catalog' element={<Catalog />} />
+
+        <Route element={<GuestGuard/>}>
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
+        </Route>
+
         <Route element={<AuthGuard/>}>
         <Route path='/logout' element={<Logout />} />
         <Route path='/add-car' element={<CreateCar/>} />
         <Route path='/cars/:carId/edit' element={<CarEdit/>} />
         </Route>
+        
         <Route path='/cars/:carId/details' element={<CarDetails/>} />
       
         <Route path='/search' element={<Search/>} />
