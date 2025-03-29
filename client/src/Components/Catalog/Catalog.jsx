097f3e5007658d2b011ba4ac8cf1733f54b55bd2
mspirovv <mspirov@abv.bg>
2025-03-29 16:16:25 +0200

    Implement pagination links in the address bar for better navigation

--- client/src/Components/Catalog/Catalog.jsx
+++ client/src/Components/Catalog/Catalog.jsx
@@ -1,37 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router";
 import { useCars } from "../../services/carService";
 import CarCatalogItem from "./car-catalog-item/CarCatalogItem";
 
 import './Catalog.css'
 
-export default function Catalog() {
-  const pageSize = 8;
-  const [currentPage,setCurrentPage] = useState(1);
-  const { cars } = useCars();
 
+export default function Catalog() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const pageSize = 8;
+
+  const { cars } = useCars();
   const totalCars = cars.length;
-  const totalPages = Math.ceil(totalCars/pageSize);
-  const displayedCars = cars.slice((currentPage-1) * pageSize,currentPage * pageSize);
-   return (
-       <>
+  const totalPages = Math.ceil(totalCars / pageSize);
+
+  const pageFromUrl = Number(searchParams.get("page")) || 1;
+  const [currentPage, setCurrentPage] = useState(pageFromUrl);
+  const displayedCars = cars.slice((currentPage - 1) * pageSize, currentPage * pageSize);
+
+  useEffect(() => {
+    if (!searchParams.has("page")) {
+      setSearchParams({ page: "1" }, { replace: true });
+    }
+  }, []);
+
+  useEffect(() => {
+    setCurrentPage(pageFromUrl);
+  }, [pageFromUrl]);
+
+  const changePage = (newPage) => {
+    setCurrentPage(newPage);
+    setSearchParams({ page: newPage.toString() });
+  }
+  return (
+    <>
       <div className="featured-cars-content">
         <div className="row">
-         
-         {displayedCars.length > 0 
-         ? displayedCars.map(car => <CarCatalogItem key={car._id}{...car} />)
-         : <h3>No added cars yet!</h3>}
+
+          {displayedCars.length > 0
+            ? displayedCars.map(car => <CarCatalogItem key={car._id}{...car} />)
+            : <h3>No added cars yet!</h3>}
         </div>
 
         <div className="pagination">
-          <button disabled={currentPage === 1} onClick={() => setCurrentPage(currentPage-1)}>
+          <button disabled={currentPage === 1} onClick={() => changePage(currentPage - 1)}>
             Previous
           </button>
           <span>Page {currentPage} of {totalPages}</span>
-          <button disabled={currentPage == totalPages} onClick={() => setCurrentPage(currentPage +1)}>
+          <button disabled={currentPage == totalPages} onClick={() => changePage(currentPage + 1)}>
             Next
           </button>
         </div>
       </div>
-       </>
-   );
+    </>
+  );
 }
