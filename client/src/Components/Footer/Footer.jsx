30bd2dc002157d45b019563da005a4d3019836ac
mspirovv <mspirov@abv.bg>
2025-03-31 21:00:00 +0300

    footer style edited

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,181 +1,64 @@
-import './Footer.css'
+import './Footer.css';
 import { Link } from 'react-router';
-export default function Footer() {
-   return (
-       <>
-      
 
-  <section id="blog" className="blog" />
-  {/*/.blog*/}
-  {/*blog end */}
-  {/*contact start*/}
-  <footer id="contact" className="contact">
-    <div className="container">
-      <div className="footer-top">
-        <div className="row">
-          <div className="col-md-3 col-sm-6">
-            <div className="single-footer-widget">
-              <div className="footer-logo">
-               
-              <Link to='/'>carvilla</Link>
-              </div>
-              <p>
-                Ased do eiusm tempor incidi ut labore et dolore magnaian aliqua.
-                Ut enim ad minim veniam.
-              </p>
-              <div className="footer-contact">
-                <p>info@themesine.com</p>
-                <p>+1 (885) 2563154554</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-2 col-sm-6">
-            <div className="single-footer-widget">
-              <h2>about devloon</h2>
-              <ul>
-                <li>
-                  <a href="#">about us</a>
-                </li>
-                <li>
-                  <a href="#">career</a>
-                </li>
-                <li>
-                  <a href="#">
-                    terms <span> of service</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">privacy policy</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-md-3 col-xs-12">
-            <div className="single-footer-widget">
-              <h2>top brands</h2>
-              <div className="row">
-                <div className="col-md-7 col-xs-6">
-                  <ul>
-                    <li>
-                      <a href="#">BMW</a>
-                    </li>
-                    <li>
-                      <a href="#">lamborghini</a>
-                    </li>
-                    <li>
-                      <a href="#">camaro</a>
-                    </li>
-                    <li>
-                      <a href="#">audi</a>
-                    </li>
-                    <li>
-                      <a href="#">infiniti</a>
-                    </li>
-                    <li>
-                      <a href="#">nissan</a>
-                    </li>
-                  </ul>
+export default function Footer() {
+  return (
+    <>
+      <footer id="contact" className="contact">
+        <div className="container">
+          <div className="footer-top">
+            <div className="row">
+      
+              <div className="col-md-4 col-sm-6">
+                <div className="single-footer-widget">
+                  <div className="footer-logo">
+                    <Link to='/'>carvilla</Link>
+                  </div>
+                  <p>
+                    Ased do eiusm tempor incidi ut labore et dolore magnaian aliqua.
+                    Ut enim ad minim veniam.
+                  </p>
+                  <div className="footer-contact">
+                    <p>info@themesine.com</p>
+                    <p>+1 (885) 2563154554</p>
+                  </div>
                 </div>
-                <div className="col-md-5 col-xs-6">
+              </div>
+
+              <div className="col-md-4 col-sm-6">
+                <div className="single-footer-widget">
+                  <h2>Quick Links</h2>
                   <ul>
-                    <li>
-                      <a href="#">ferrari</a>
-                    </li>
-                    <li>
-                      <a href="#">porsche</a>
-                    </li>
-                    <li>
-                      <a href="#">land rover</a>
-                    </li>
-                    <li>
-                      <a href="#">aston martin</a>
-                    </li>
-                    <li>
-                      <a href="#">mersedes</a>
-                    </li>
-                    <li>
-                      <a href="#">opel</a>
-                    </li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/catalog">Catalog</Link></li>
+                    <li><Link to="/search">Search</Link></li>
+                    
                   </ul>
                 </div>
               </div>
+
+              <div className="col-md-4 col-sm-6">
+                <div className="single-footer-widget">
+                  <h2>Follow Us</h2>
+                  <div className="footer-social">
+                    <a href="#"><i className="fa fa-facebook" /></a>
+                    <a href="#"><i className="fa fa-instagram" /></a>
+                    <a href="#"><i className="fa fa-linkedin" /></a>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="col-md-offset-1 col-md-3 col-sm-6">
-            <div className="single-footer-widget">
-              <h2>news letter</h2>
-              <div className="footer-newsletter">
-                <p>Subscribe to get latest news update and informations</p>
+
+          <div className="footer-copyright">
+            <div className="row">
+              <div className="col-sm-12 text-center">
+                <p>© copyright SoftUni React Exam March 2025</p>
               </div>
-              <div className="hm-foot-email">
-                <div className="foot-email-box">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Add Email"
-                  />
-                </div>
-                {/*/.foot-email-box*/}
-                <div className="foot-email-subscribe">
-                  <span>
-                    <i className="fa fa-arrow-right" />
-                  </span>
-                </div>
-                {/*/.foot-email-icon*/}
-              </div>
-              {/*/.hm-foot-email*/}
             </div>
           </div>
         </div>
-      </div>
-      <div className="footer-copyright">
-        <div className="row">
-          <div className="col-sm-6">
-            <p>
-              © copyright.designed and developed by{" "}
-              <a href="https://www.themesine.com/">themesine</a>.
-            </p>
-            {/*/p*/}
-          </div>
-          <div className="col-sm-6">
-            <div className="footer-social">
-              <a href="#">
-                <i className="fa fa-facebook" />
-              </a>
-              <a href="#">
-                <i className="fa fa-instagram" />
-              </a>
-              <a href="#">
-                <i className="fa fa-linkedin" />
-              </a>
-              <a href="#">
-                <i className="fa fa-pinterest-p" />
-              </a>
-              <a href="#">
-                <i className="fa fa-behance" />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/*/.footer-copyright*/}
-    </div>
-    {/*/.container*/}
-    <div id="scroll-Top">
-      <div className="return-to-top">
-        <i
-          className="fa fa-angle-up "
-          id="scroll-top"
-          data-toggle="tooltip"
-          data-placement="top"
-          title=""
-          data-original-title="Back to Top"
-          aria-hidden="true"
-        />
-      </div>
-    </div>
-    {/*/.scroll-Top*/}
-  </footer>
-       </>
-   );
+      </footer>
+    </>
+  );
 }
